1f0ab154d46a49bce2fcab98984dcf35a98ec034
Administrator <Administrator@6SYKVV4VAB4N3IZ>
2019-03-19 18:49:58 +0800

    位图数据结构

--- dataStructure001/src/main/java/com/navy/bitmap/BitMap.java
+++ dataStructure001/src/main/java/com/navy/bitmap/BitMap.java
@@ -2,36 +2,55 @@ package com.navy.bitmap;
 
 public class BitMap {
 	
-	private byte[] bytes;
+	private char[] chars;
 	private int bitSize;
 	
 	public BitMap(int bitSize) {
 		//一个char是8 bit
 		this.bitSize = bitSize;
-		this.bytes = new byte[bitSize/8 + 1];
+		this.chars = new char[bitSize/16 + 1];
 	}
 	
 	public void setBit(int index) {
-		int byteIndex = index / 8;
-		int bitIndex = index % 8;
-		bytes[byteIndex] |= 1 << bitIndex;
+		if(index >= bitSize) {
+			return;
+		}
+		int byteIndex = index / 16;
+		int bitIndex = index % 16;
+		chars[byteIndex] |= 1 << bitIndex;
 		//number << 1; number向左移动一位，即在右边用0填充
 		//1010向左移动一位，结果为10100
-		//a|=b，a与b或操作后，将值赋予a
+		//a|=b，a与b进行或操作后，将值赋予a
 		//举例：00000001 |= 00000010，结果为00000011
 	}
 	
+	public boolean get(int index) {
+		if(index >= bitSize) {
+			return false;
+		}
+		
+		int byteIndex = index / 16;
+		int bitIndex = index % 16;
+		//number << 1; number向左移动一位，即在右边用0填充
+		//((00000100 & (00000001 << 2)) ！= 0)运算讲解：
+		//1）00000001 << 2，即00000001向左移动两位，即在00000001的右边补两个0，为00000100；
+		//2）00000100 & 00000100,&为与操作，结果为00000100
+		//3）00000100 ！= 0，结果为true
+		boolean result = (chars[byteIndex] & (1<<bitIndex)) != 0;
+		return result;
+	}
+	
 	public void print() {
-		for(byte b : bytes) {
+		for(char b : chars) {
 			print(b);
 		}
 	}
 	
-	private void print(byte b) {
-		String result = Integer.toBinaryString((int)b);
+	private void print(char b) {
+		String result = Integer.toBinaryString(b);
 		int len = result.length();
-		if(len < 8) {
-			for(int i = 0; i < 8-len; i++) {
+		if(len < 16) {
+			for(int i = 0; i < 16-len; i++) {
 				result = "0" + result;
 			}
 		}
--- dataStructure001/src/test/java/com/navy/bitmap/BitMapTest.java
+++ dataStructure001/src/test/java/com/navy/bitmap/BitMapTest.java
@@ -1,5 +1,7 @@
 package com.navy.bitmap;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class BitMapTest {
@@ -7,6 +9,19 @@ public class BitMapTest {
 	@Test
 	public void testSetBit() {
 		BitMap bitMap = new BitMap(8);
-		bitMap.print();
+		for(int i = 0; i < 16; i++) {
+			bitMap.setBit(i);
+			bitMap.print();
+			System.out.println("===============");
+		}
+	}
+	
+	@Test
+	public void testGet() {
+		BitMap bitMap = new BitMap(8);
+		int index = 2;
+		bitMap.setBit(index);
+		boolean result = bitMap.get(index);
+		assertEquals(true, result);
 	}
 }
